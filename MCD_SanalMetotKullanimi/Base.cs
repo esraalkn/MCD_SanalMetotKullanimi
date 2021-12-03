5641b22ceea823f8357140d7fbd3547781057765
Esra Alkan <es.alknn@gmail.com>
2021-12-03 17:16:55 +0300

    Add project files.

---
+++ MCD_SanalMetotKullanimi/Base.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetotKullanimi
+{
+   public class BaseClass
+    {
+        public BaseClass()
+        {
+            Console.WriteLine("base class");
+        }
+
+        public virtual void ekranaYaz(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
---
+++ MCD_SanalMetotKullanimi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetotKullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Televizyon T1 = new Televizyon();
+            T1.ekranaYaz("Merhaba");
+            Console.WriteLine("******************");
+            Urun U1 = new Urun();
+            U1.ekranaYaz("Merhaba");
+            Console.WriteLine("*******************");
+            BaseClass B1 = new BaseClass();
+            B1.ekranaYaz("Merhaba");
+            Console.ReadKey();
+        }
+    }
+}
